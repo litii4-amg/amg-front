ab5691be63d80abbe1db38617ed58937bbba5d6d
S-Pz <saviofrancisco150@hotmail.com>
2025-02-20 14:41:38 -0300

    adding some filds in modal

--- src/js/layouts/Navbar.js
+++ src/js/layouts/Navbar.js
@@ -1,3 +1,5 @@
+import React, {useState, useEffect, use} from 'react';
+
 import {Link} from "react-router-dom"
 
 import Container from './Container';
@@ -6,16 +8,32 @@ import styles from "../../css/Navbar.module.css"
 
 import logo from "../../img/AMG-logo-blue.svg"
 
+import { IoIosArrowDown } from "react-icons/io";
+
 function Navbar(){
+    const [dropdownOpen, setDropdownOpen] = useState(false);
+    
     return(
         <nav className={styles.navbar}>
             <Container>
                 <Link to="/"><img src={logo} alt="AMG"/></Link>
                 <ul className={styles.list}>
                     <li className={styles.item}><Link to="/">Inicio</Link></li>
-                    <li className={styles.item}><Link to="/pedido">Pedido</Link></li>
+                    
+                    <li className={styles.dropdown} onMouseEnter={() => setDropdownOpen(true) } onMouseLeave={() => setDropdownOpen(false)}>
+                    <span className={styles.dropbtn}>Produção <IoIosArrowDown className={styles.IoIosArrowDown}/></span>
+                    
+                    {dropdownOpen && (
+                        <ul className={styles.dropdown_content}>
+                            <li><Link to="/pedido">Pedido</Link></li>
+                            <li><Link to="/lote">Lote</Link></li>
+                            <li><Link to ="/produto">Produto</Link></li>
+                        </ul>
+                    )}
+                    </li>
+                    {/* <li className={styles.item}><Link to="/pedido">Pedido</Link></li>
                     <li className={styles.item}><Link to="/lote">Lote</Link></li>
-                    <li className={styles.item}><Link to ="/produto">Produto</Link></li>
+                    <li className={styles.item}><Link to ="/produto">Produto</Link></li> */}
                 </ul>
             </Container>
         </nav>
--- src/js/projectForms/ProdutoModal.js
+++ src/js/projectForms/ProdutoModal.js
@@ -2,26 +2,61 @@ import React, {useState, useEffect, use} from 'react';
 
 import styles from './ProdutoModal.module.css';
 
-function ProdutoModal({isOpen, onClose}) {
-    if (!isOpen) return null;
+function ProdutoModal({isOpen, onClose, produto}) {
+    if (!isOpen || !produto) return null;
     
     return (
         <div className={styles.window}>
             <div className={styles.window_content}>
                 <button className={styles.close_button} onClick={onClose}>&times;</button>
-                <form> 
-                    <label>Código</label>
-                    <input type="text" name="cod" />
-                    <label>Tipo</label>
-                    <input type="text" name="tipo" />
-                    <label>Código da linha</label>
-                    <input type="text" name="codLinha" />
-                    <label>Nome</label>
-                    <input type="text" name="nome" />
-                    <label>Acabamento</label>
-                    <input type="text" name="acabamento" />
-                    <label>Composição</label>
-                </form>
+                <div className={styles.form_group}>
+                    <label>Código:</label>
+                    <span>{produto.cod}</span>
+                </div>
+                <div className={styles.form_group}>
+                    <label>Descrição:</label>
+                    <textarea name="descricao" defaultValue={produto.descricao} rows={3} />
+                </div>
+                <div className={styles.form_group}>
+                    <label>Tipo:</label>
+                    <input type="text" name="tipo" defaultValue={produto.tipo} />
+                </div>
+                <div className={styles.form_group}>
+                    <label>Código da linha:</label>
+                    <span>{produto.codLinha}</span>
+                </div>
+                <div className={styles.form_group}>
+                    <label>Rastro:</label>
+                    <input type="text" name="rastro" defaultValue={produto.rastro} />
+                </div>
+                <div className={styles.form_group}>
+                    <label>Metal:</label>
+                    <input type="text" name="metal" defaultValue={produto.metal} />
+                </div>
+                <div className={styles.form_group}>
+                    <label>Refusão:</label>
+                    <input type="text" name="refusao" defaultValue={produto.refusao} />
+                </div>
+                <div className={styles.form_group}>
+                    <label>Componente:</label>
+                    <input type="text" name="cod_componente" defaultValue={produto.cod_componente} />
+                </div>
+                <div className={styles.form_group}>
+                    <label>Descrição do Componente:</label>
+                    <input type="text" name="desc_componente" defaultValue={produto.desc_componente} />
+                </div>
+                <div className={styles.form_group}>
+                    <label>Quantidade do Componente:</label>
+                    <input type="text" name="qtd_componente" defaultValue={produto.qtd_componente} />
+                </div>
+                <div className={styles.form_group}>
+                    <label>Nome:</label>
+                    <input type="text" name="nome" defaultValue={produto.nome} />
+                </div>
+                <div className={styles.form_group}>
+                    <label>Acabamento:</label>
+                    <input type="text" name="acabamento" defaultValue={produto.acabamento} />
+                </div>
             </div>
         </div>
        
